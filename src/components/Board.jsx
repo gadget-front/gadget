bcb12f6574d65f8b31c7703ec1b126638e1fe9fb
mxzo <mxzo@naver.com>
2023-02-02 15:38:57 +0900

    백 연결

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Board = () => {
+  const [message, setMessage] = useState([]);
+  useEffect(() => {
+    fetch("gadget/hello")
+      .then((res) => res.json())
+      .then((data) => setMessage(data));
+  }, []);
   return (
-    <div>Board</div>
+    <div>Board
+      <ul>
+        {message.map((v, idx) => (
+          <li key={`${idx}-${v}`}>{v}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
