44890b8e5653882e69a08baf6144a404732df239
mxzo <mxzo@naver.com>
2023-02-13 09:43:49 +0900

    충돌해결 브랜치

--- src/index.js
+++ src/index.js
@@ -1,54 +1,25 @@
-import React, {Suspense} from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Route, Routes } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
-import Login from './components/Login';
-import PrivateRoute from './components/PrivateRouter';
-import { GoogleLogin, GoogleOAuthProvider } from '@react-oauth/google';
 
 const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById('root'));
-// const token = sessionStorage.getItem("Authorization");
-
-// console.log("token :" + token);
-
 root.render(
   // <React.StrictMode>
-  <GoogleOAuthProvider clientId='500664645859-g132g5tgks2cvsvsph5q8rbuq0o8sivv.apps.googleusercontent.com'> 
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <Suspense fallback={<div>loading....</div>}>
-          <Routes>
-            <Route path='/login' element={<Login />}/>
-            {/* <Route path="/*" element={<PrivateRoute component={<App/>} authenticated={token}/>}/> */}
-            <Route path="/*" element={<PrivateRoute component={<App/>}/>}/>
-          </Routes>
-        </Suspense>
-        <ReactQueryDevtools initialIsOpen />
-      </BrowserRouter>   
-    </QueryClientProvider>
-  </GoogleOAuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+      <ReactQueryDevtools initialIsOpen />
+    </BrowserRouter>   
+  </QueryClientProvider>
   // </React.StrictMode> 
 
 );
-
-/* <Suspense fallback={<div>loading....</div>}>
-            <Routes>          
-                <Route path='/todo-list' element={<TodoList />}/>
-                <Route path="/todoList/:contentId" element={<TodoDetail/>} />
-                <Route path="/makeTodo/:state" element={<MakeTodoContent/>} />
-                <Route path='/board' element={<Board />}/>
-                <Route path= {pathdetail} element={<BoardDetail />} />
-                <Route path='/chatting' element={<Chatting />} />
-                <Route path='/calender' element={<Calender />} />
-                <Route path='*' element={<NoChice />} />
-            </Routes>
-        </Suspense> */
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
